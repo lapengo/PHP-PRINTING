8e7c36f6a83894ff3d40a629e28dfbf3ac046a09
lapengo <mylapengo@gmail.com>
2020-06-03 20:54:43 +0700

    Looping

--- looping.php
+++ looping.php
@@ -46,3 +46,85 @@ for ($i=0; $i < 6; $i++) {
    }
    echo "<br/>";
 }
+
+echo "<h2>4A. Triangle Start pattern</h2>"; 
+
+$row = 5;
+
+for ($i=1; $i <= $row; $i++) { 
+   for ($j=$i; $j < $row; $j++) { 
+      echo"&nbsp;&nbsp;";
+   }
+
+   for ($k=1; $k <= (2 * $i) ; $k++) { 
+     if($k == 1 || $i == $row || $k == (2 * $i - 1)){
+        echo"*";
+     }else{
+      echo"&nbsp;&nbsp;";
+     }
+   }
+   echo "<br/>";
+}
+
+echo "<h2>4B. Triangle Start pattern</h2>"; 
+
+$row = 5;
+
+for ($i=$row; $i >= 1; $i--) { 
+   for ($j=$i; $j < $row; $j++) { 
+      echo"&nbsp;&nbsp;";
+   }
+
+   for ($k=1; $k <= (2 * $i) ; $k++) { 
+     if($k == 1 || $i == $row || $k == (2 * $i - 1)){
+        echo"*";
+     }else{
+      echo"&nbsp;&nbsp;";
+     }
+   }
+   echo "<br/>";
+}
+
+echo "<h2>5. Diamond Pattern</h2>"; 
+
+for ($i=1; $i <= $row ; $i++) { 
+   for ($k=$row; $k > $i ; $k--) { 
+      echo"&nbsp;&nbsp;";
+   }
+
+   echo"*";
+
+   for ($j=1; $j < 2 * ($i - 1) ; $j++) { 
+      echo"&nbsp;&nbsp;";
+   }
+
+   if($i == 1){
+      echo"&nbsp;&nbsp;";
+   }else{
+      echo "*";
+   }
+
+   echo"<br/>";
+
+}
+
+for ($i=$row; $i >= 1 ; $i--) { 
+   for ($k=$row; $k > $i ; $k--) { 
+      echo"&nbsp;&nbsp;";
+   }
+
+   echo"*";
+
+   for ($j=1; $j < 2 * ($i - 1) ; $j++) { 
+      echo"&nbsp;&nbsp;";
+   }
+
+   if($i == 1){
+      echo"&nbsp;&nbsp;";
+   }else{
+      echo "*";
+   }
+
+   echo"<br/>";
+
+}
